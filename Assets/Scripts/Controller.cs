6e920e18b5aef8a086d653cd4e33de67a80e2581
KyleVanHook <141291970+KyleVanHook@users.noreply.github.com>
2025-10-02 20:42:29 -0700

    Modified the movement and added arrow key teleportation.
    Modified the movement so that it detects that either the Left or Right shift key is held down during movement in order to activate a boost. I also made it so the arrow keys would teleport the player in the direction of the arrow key, based on the sprites current orientation. Also moved the random teleport functions and variables form the SpritMover into the controller and pawn scripts.

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -20,35 +20,86 @@ public class Controller : MonoBehaviour
 
     private void MakeDecisions()
     {
-        //Move Forward
+        //Movement Stuff
+        //Move Forward when holding the W key
         if(Input.GetKey(KeyCode.W))
         {
-
-            //Tell pawn to Move Forward on their own moveSpeed
-            pawn.MoveForward(pawn.moveSpeed);
-            
+            //if the Left OR Right shift is held down, use the TurboSpeed variable for the move speed.
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                //Tell pawn to Move Forward using the TurboSpeed
+                pawn.MoveForward(pawn.turboSpeed);
+            }
+            else
+            {
+                //Tell pawn to Move Forward on their own moveSpeed
+                pawn.MoveForward(pawn.moveSpeed);
+            }
         }
 
-        //Move Back
+        //Move Back when holding the S key
         if (Input.GetKey(KeyCode.S))
         {
-            //Tell pawn to Move Backwards on their own moveSpeed
-            pawn.MoveBack(pawn.moveSpeed);
+            //if the Left OR Right shift is held down, use the TurboSpeed variable for the move speed.
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                //Tell pawn to Move Back using the TurboSpeed
+                pawn.MoveBack(pawn.turboSpeed);
+            }
+            else
+            {
+                //Tell pawn to Move Backwards on their own moveSpeed
+                pawn.MoveBack(pawn.moveSpeed);
+            }
         }
 
-        //Rotate Clockwise
+        //Rotate Clockwise when holding the D key
         if (Input.GetKey(KeyCode.D))
         {
             //Tell pawn to Rotate Clockwise on their own moveSpeed
             pawn.RotateClockwise(pawn.rotateValue);
         }
 
-        //Rotate CounterClockwise
+        //Rotate CounterClockwise when holding the A key
         if (Input.GetKey(KeyCode.A))
         {
             //Tell pawn to Rotate CounterClockwise on their own moveSpeed
             pawn.RotateCounterClockwise(pawn.rotateValue);
         }
 
+
+
+        //Teleport stuff
+        //If the T key is pressed, teleport the sprite randomly
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            pawn.TeleportRandom(pawn.randomTeleportRange_X, pawn.randomTeleportRange_Y);
+        }
+
+
+
+        //if the up arrow key is pressed, telport the sprite forward
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            pawn.TeleportForward(pawn.moveSpeed);
+        }
+
+        //if the up arrow key is pressed, telport the sprite backwards
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            pawn.TeleportBack(pawn.moveSpeed);
+        }
+
+        //if the up arrow key is pressed, telport the sprite to the left
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            pawn.TeleportLeft(pawn.moveSpeed);
+        }
+
+        //if the up arrow key is pressed, telport the sprite to the right
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            pawn.TeleportRight(pawn.moveSpeed);
+        }
     }
 }
--- Assets/Scripts/Pawn.cs
+++ Assets/Scripts/Pawn.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Pawn : MonoBehaviour
 {
+    private Transform spriteTransform;
+    public float randomTeleportRange_X;
+    public float randomTeleportRange_Y;
+
     public float moveSpeed;
     public float rotateValue;
-    //public float turboSpeed;
+    public float turboSpeed;
+
+    //teleport range for arrow keys
+   // public float teleportRange;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //Get the transform for this sprit
+        spriteTransform = this.gameObject.GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -49,6 +58,47 @@ public class Pawn : MonoBehaviour
         //Get the transform component
         //Deltatime makes it so that our speed is not tied to frame rate. it is now angles per second.
         transform.Rotate(0.0f, 0.0f, rotateValue * Time.deltaTime);
+    }
+
+
+    // Teleport the sprit to a random space on the field.
+    public void TeleportRandom(float randomTeleportRange_X, float randomTeleportRange_Y)
+    {
+        //Check that the spriteTransform is not Null.
+        //if true, set the X & Y positions in the transform to a random number between the teleport range variable (Variable must be positive!).
+        if (spriteTransform != null)
+        { 
+            spriteTransform.position = new Vector3(Random.Range(-randomTeleportRange_X, randomTeleportRange_X), Random.Range(-randomTeleportRange_Y, randomTeleportRange_Y), 0.0f);
+        }
+    }
+
+
+    //Teleport to a new position in front of the sprite. Uses the movespeed to determine the teleport range.
+    //Getting rid of the transform.position in the formula will make it so it transforms to the global position, not the local position. 
+    public void TeleportForward(float moveSpeed)
+    {
+        transform.position = transform.position + (transform.up * moveSpeed);
+    }
+
+    //Teleport to a new position in the back of the sprite. Uses the movespeed to determine the teleport range.
+    //Getting rid of the transform.position in the formula will make it so it transforms to the global position, not the local position. 
+    public void TeleportBack(float moveSpeed)
+    {
+        transform.position = transform.position + (transform.up * -moveSpeed);
+    }
+
+    //Teleport to a new position left of the sprite. Uses the movespeed to determine the teleport range.
+    //Getting rid of the transform.position in the formula will make it so it transforms to the global position, not the local position. 
+    public void TeleportLeft(float moveSpeed)
+    {
+        transform.position = transform.position + (transform.right * -moveSpeed);
+    }
+
+    //Teleport to a new position Right of the sprite. Uses the movespeed to determine the teleport range.
+    //Getting rid of the transform.position in the formula will make it so it transforms to the global position, not the local position. 
+    public void TeleportRight(float moveSpeed)
+    {
+        transform.position = transform.position + (transform.right * moveSpeed);
     }
 }
 
